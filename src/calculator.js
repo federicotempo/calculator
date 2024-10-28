c28469f9fa34ab0f28632491f711e948d1802fdd
federicotempo <federicotempo98@gmail.com>
2024-10-28 17:59:02 -0300

    Creates operate
    This commit creates a new function "operate" that takes an operator and 2 numbers and then calls one of the above functions on the numbers.

--- src/calculator.js
+++ src/calculator.js
@@ -15,6 +15,30 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(firstNumber, secondNumber, operator) {
+    let result = 0;
+
+    switch (operator) {
+        case "+":
+            result = add(firstNumber, secondNumber);
+            break;
+
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+
+        case "*":
+            result = multiply(firstNumber, secondNumber);
+            break;
+
+        case "/":
+            result = divide(firstNumber, secondNumber);
+            break;
+    }
+
+    return result;
+}
+
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "+";
