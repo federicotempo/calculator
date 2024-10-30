caeab7741764b75ee3106ed4cc1acdc93f7b2e80
federicotempo <federicotempo98@gmail.com>
2024-10-30 15:56:42 -0300

    Creates equalButton, operatorButtons. Modifies displayValue
    This commit creates equalButton, operatorButtons and modifies displayValue function.

--- src/calculator.js
+++ src/calculator.js
@@ -41,36 +41,73 @@ function operate(firstNumber, secondNumber, operator) {
 
 function displayValue(value) {
 
-    if (displayedValue.innerText.length < 10) {
-        if (displayedValue.innerText === "0" && value === ".") {
-            displayedValue.innerText += value;
-        } else if (displayedValue.innerText.includes(".") && value === ".") {
-            return
-        } else if (displayedValue.innerText === "0") {
-            displayedValue.innerText = value;
-        } else {
-            displayedValue.innerText += value;
+    if (operator !== "") {
+        displayedValue.innerText = "0";
+        if (displayedValue.innerText.length < 10) {
+            if (displayedValue.innerText === "0" && value === ".") {
+                displayedValue.innerText += value;
+                secondNumber = Number(displayedValue.innerText);
+            } else if (displayedValue.innerText.includes(".") && value === ".") {
+                return
+            } else if (displayedValue.innerText === "0") {
+                displayedValue.innerText = value;
+                secondNumber = Number(displayedValue.innerText);
+            } else {
+                displayedValue.innerText += value;
+                secondNumber = Number(displayedValue.innerText);
+            }
+        }
+    } else {
+        if (displayedValue.innerText.length < 10) {
+            if (displayedValue.innerText === "0" && value === ".") {
+                displayedValue.innerText += value;
+                firstNumber = Number(displayedValue.innerText);
+            } else if (displayedValue.innerText.includes(".") && value === ".") {
+                return
+            } else if (displayedValue.innerText === "0") {
+                displayedValue.innerText = value;
+                firstNumber = Number(displayedValue.innerText);
+            } else {
+                displayedValue.innerText += value;
+                firstNumber = Number(displayedValue.innerText);
+            }
         }
     }
-
-
 
 }
 
 function selectNumericButtons() {
-    let selection = this.innerText;
-    displayValue(selection);
+    let selectedNumber = this.innerText;
+    displayValue(selectedNumber);
+}
+
+function selectOperator() {
+    let selectedOperator = this.innerText;
+    operator = selectedOperator;
 }
 
 const displayedValue = document.querySelector("#value");
 
 let firstNumber = 0;
 let secondNumber = 0;
-let operator = "+";
+let operator = "";
 
+let operatorButtons = document.querySelectorAll(".operator-button")
 let numericButtons = document.querySelectorAll(".numeric-button");
+let equalButton = document.querySelector("#equal-button");
+
+equalButton.addEventListener("click", () => {
+    if (operator !== "") {
+        operate(firstNumber, secondNumber, operator);
+    }
+})
 
 numericButtons.forEach(button => {
     button.addEventListener("click", selectNumericButtons);
 })
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", selectOperator);
+})
+
 
